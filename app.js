3a145ce44c44607e8b9158b8550ca614daf4b35f
Mathew Thomas <mathewthomas@Mathews-MacBook-Pro.local>
2023-04-04 13:03:37 -0400

    more changes

--- app.js
+++ app.js
@@ -1,53 +1,56 @@
 function randomInt(n) {
-    return Math.floor(Math.random() * n) + 1;
+  return Math.floor(Math.random() * n) + 1;
+}
+
+function parseInput(string) {
+  if (string === "") {
+    return NaN;
+  } else if (string === null) {
+    return null;
+  } else {
+    return Number(string);
   }
-  
-  function parseInput(string) {
-    if (string === "") {
-      return NaN;
-    } else if (string === null) {
-      return null;
+}
+function promptInt(message) {
+  let number = parseInput(prompt(message));
+
+  while (!Number.isInteger(number) && number !== null) {
+    number = parseInput(prompt("Please enter an integer."));
+  }
+
+  return number;
+}
+
+function play() {
+  const secretNumber = randomInt(10);
+  let playerName = prompt("Enter your name here");
+  let guess = promptInt(`Guess a number ${playerName}.`);
+  let guesses = [guess];
+
+  while (guess !== secretNumber) {
+    if (guess === null) {
+      alert("Goodbye!");
+      return;
+    }
+
+    if (guess < secretNumber) {
+      guess = promptInt(`Sorry ${playerName}, Guess higher!`);
     } else {
-      return Number(string);
+      guess = promptInt(`Sorry ${playerName}, Guess lower!`);
     }
+
+    guesses.push(guess);
   }
-  function promptName (message) {
-      let player = promptName(prompt(message))
-  
+
+  alert(`Correct! ${playerName} previous attempts were: ${guesses}.`);
+  let playAgain = prompt(
+    `would you like to play again ${playerName}! Enter yes or no.`
+  );
+  if (playAgain.tolowercase == "yes") {
+    play();
+  } else {
+    alert(`goodbye ${playerName}`);
   }
-  function promptInt(message) {
-    let number = parseInput(prompt(message));
-  
-    while (!Number.isInteger(number) && number !== null) {
-      number = parseInput(prompt("Please enter an integer."));
-    }
-  
-    return number;
-  }
-  
-  function play() {
-    const secretNumber = randomInt(10);
-    let playerName = promptName('Enter your name here')
-    let guess = promptInt("Guess a number.");
-    let guesses = [guess];
-  
-    while (guess !== secretNumber) {
-      if (guess === null) {
-        alert("Goodbye!");
-        return;
-      }
-  
-      if (guess < secretNumber) {
-        guess = promptInt(`Guess higher!`);
-      } else {
-        guess = promptInt("Guess lower!");
-      }
-  
-      guesses.push(guess);
-    }
-  
-    alert(`Correct! ${playerName} previous attempts were: ${guesses}.`);
-    
-  }
-  
-  play();
+}
+
+play();
